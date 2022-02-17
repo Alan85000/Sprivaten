e2e5ead6e5da805575e87916017d494e536a235a
Simon7644 <simonbonnet76@gmail.com>
2022-02-17 09:35:57 +0100

    Ajout du header.php

---
+++ tpgit/sprivaten/header.php
@@ -0,0 +1,17 @@
+<header>
+<nav>
+    <h1>Sprivaten</h1>
+    <ul>
+        <li>
+            <a href="home">Home</a>
+            <a href="product">Product</a>
+            <a href="pricing">Pricing</a>
+            <a href="contact">Contact</a>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-cart-shopping"></i>
+            <i class="fa-solid fa-bars"></i>
+        </li>
+    </ul>
+
+</nav>
+</header>
